e6b529932a19ef7cc1600c6c73de3322b245cca2
florianhirson <flohirson@gmail.com>
2022-09-30 09:45:35 +0200

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'printing-quoting-sheet';
+
+  saleData = [
+    { name: "Preparation", value: 21.4 },
+    { name: "Filament", value: 34.5 },
+    { name: "Electricity", value: 2.3 },
+    { name: "Printer depreciation", value: 23.8 },
+    { name: "Post processing", value: 17.9 }
+  ];
+
+  labelFormatting(name: string) { // this name will contain the name you defined in chartData[]
+    let self: any = this; // this "this" will refer to the chart component (pun intented :))
+
+    let data = self.series.filter((x: { name: string; }) => x.name == name); // chartData will be present in
+                                                        // series along with the label
+
+    if(data.length > 0) {
+      return `${data[0].name}: ${data[0].value} %`;
+    } else {
+      return name;
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,33 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatButtonModule} from "@angular/material/button";
+import {MatTabsModule} from "@angular/material/tabs";
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import {MatGridListModule} from "@angular/material/grid-list";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {MatDatepickerModule} from "@angular/material/datepicker";
+import {MatNativeDateModule} from '@angular/material/core';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatTabsModule,
+    NgxChartsModule,
+    MatGridListModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule
   ],
   providers: [],
   bootstrap: [AppComponent]
