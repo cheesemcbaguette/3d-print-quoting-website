8cf8ec2ec73d386a7a83c49aee66ede86e5eba8f
cheesemcbaguette <29225021+cheesemcbaguette@users.noreply.github.com>
2024-08-15 15:25:54 +0200

    Revert .gitignore changes Build website content to /docs
    Signed-off-by: cheesemcbaguette <29225021+cheesemcbaguette@users.noreply.github.com>

---
+++ docs/assets/currencies-data.ts
@@ -0,0 +1,16 @@
+import {Currency} from "../app/model/currency";
+
+export const CURRENCIES: Currency[] = [
+  {
+    symbol: "€",
+    code: "EUR"
+  },
+  {
+    symbol: "$",
+    code: "USD"
+  },
+  {
+    symbol: "£",
+    code: "GBP"
+  },
+];
